ef0e6384602627d4c8e7baef77838b4132436ecf
Yasmin Torres <yasmintorres90@hotmail.com>
2018-12-29 01:19:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,5 +11,6 @@ class Triangle
   def valid?
     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_one = @triangle_sides[]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+    
 end
